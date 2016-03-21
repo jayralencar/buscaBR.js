6f13e16b4c0a0bb6f4d2455de654951402b6b902
Rodolfo Silva <contato@rodolfosilva.com>
2016-03-20 21:47:22 -0300

    Trocando os tests atuais por teste com o Mocha

--- test/test.js
+++ test/test.js
@@ -1,7 +1,21 @@
-var buscaBR = require('../buscaBR');
+const expect = require('chai').expect;
+const buscaBR = require('../buscaBR');
 
-buscaBR.search('feijaum',['arroz','feijão','feijoada'],function(res){
-	console.log(res);
+describe('Busca BR', function() {
+	describe("Buscar", function() {
+		it("Nome errado", function(done) {
+			buscaBR.search('feijaum',['arroz','feijão','feijoada'],function(res){
+				expect(res).with.length(1);
+				expect(res[0]).to.have.property('termo').with.equal("feijão");
+				expect(res[0]).to.have.property('index').with.equal(1);
+				done();
+			});
+		});
+	});
+
+	describe("Encode", function() {
+		it("Nome com acentos", function() {
+			expect(buscaBR.encode('Feijão')).to.equal('FJ');
+		});
+	});
 });
-
-console.log(buscaBR.encode('Feijão'));
