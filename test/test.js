aaebf76adadba14ee1dcd9a3f223a3ea85aa6ac3
Rodolfo Silva <contato@rodolfosilva.com>
2016-03-20 22:17:47 -0300

    Ajustes na arquitetura e adicionando testes

--- test/test.js
+++ test/test.js
@@ -2,20 +2,20 @@ const expect = require('chai').expect;
 const buscaBR = require('../buscaBR');
 
 describe('Busca BR', function() {
-	describe("Buscar", function() {
-		it("Nome errado", function(done) {
-			buscaBR.search('feijaum',['arroz','feijão','feijoada'],function(res){
-				expect(res).with.length(1);
-				expect(res[0]).to.have.property('termo').with.equal("feijão");
-				expect(res[0]).to.have.property('index').with.equal(1);
-				done();
-			});
-		});
-	});
+  describe("Buscar", function() {
+    it("Nome errado", function(done) {
+      buscaBR.search('feijaum',['arroz','feijão','feijoada'],function(res){
+        expect(res).with.length(1);
+        expect(res[0]).to.have.property('termo').with.equal("feijão");
+        expect(res[0]).to.have.property('index').with.equal(1);
+        done();
+      });
+    });
+  });
 
-	describe("Encode", function() {
-		it("Nome com acentos", function() {
-			expect(buscaBR.encode('Feijão')).to.equal('FJ');
-		});
-	});
+  describe("Encode", function() {
+    it("Nome com acentos", function() {
+      expect(buscaBR.encode('Feijão')).to.equal('FJ');
+    });
+  });
 });
